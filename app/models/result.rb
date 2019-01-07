5d2cb34502c5768465cff932fa0c81deaf878ee6
Lukunin L. Kirill <lukunin.kirill199@gmail.com>
2019-01-07 18:57:48 +0300

    Добавил валидации

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,2 +1,5 @@
 class Result < ApplicationRecord
+#  validates :param, uniqueness: true
+  validates :param, presence: true
+  validates :res, presence: true
 end
--- test/models/result_test.rb
+++ test/models/result_test.rb
@@ -8,6 +8,8 @@ class ResultTest < ActiveSupport::TestCase
     assert posled.save
   end
 
+
+=begin
   test 'should not save duplicate data' do
     @param = '1 2 3 3 4 5 6 7 8 1 2 3 3 3 3 34 35 72 96 15 35 46 73'
     @result = [[3,4,5,6,7,8], [3,34,35,72,96], [15,35,46,73], [1,2,3], [1,2,3]]
@@ -19,4 +21,20 @@ class ResultTest < ActiveSupport::TestCase
       assert true
     end
   end
+
+
+  test 'value should be unique' do
+    test_rec = Result.new param: '1 2 3', res: '1 2 3'
+    assert test_rec.save
+    test_rec = Result.new param: '1 2 3', res: '1 2 3'
+    assert_not test_rec.save
+  end
+=end
+test 'value should be' do
+  test_rec = Result.new param: '1 2 3', res: '1 2 3'
+  assert test_rec.save
+  test_rec = Result.new param: nil, res: nil
+  assert_not test_rec.save
+end
+
 end
